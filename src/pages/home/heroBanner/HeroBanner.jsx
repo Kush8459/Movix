7a7289e10f001035143404fec6fadbe606a8cf38
Kush8459 <agrawalkush04@gmail.com>
2024-02-21 18:33:14 +0530

    Movies rating website completed

---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import "./style.scss";
+import useFetch from "../../../hooks/useFetch";
+import Img from "../../../components/lazyLoadImage/Img";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+
+const HeroBanner = () => {
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  const { url } = useSelector((state) => state.home);
+
+  const { data, loading } = useFetch("/movie/upcoming");
+
+  useEffect(() => {
+    const bg =
+      url.backdrop +
+      data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    setBackground(bg);
+  }, [data, url.backdrop]);
+
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+
+  const searchQueryHandlerBtn = (event) => {
+    event.preventDefault();
+    if (query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+
+  return (
+    <div className="heroBanner">
+      {!loading && (
+        <div className="backdrop-img">
+          <Img key={background} src={background} />
+        </div>
+      )}
+      <div className="opacity-layer"></div>
+      <ContentWrapper>
+        <div className="heroBannerContent">
+          <span className="title">Welcome.</span>
+          <span className="subTitle">
+            Millions of movies, TV shoes and people to discover. Explore now.
+          </span>
+          <form className="searchInput" onSubmit={searchQueryHandlerBtn}>
+            <input
+              type="text"
+              placeholder="Search for a movie or tv show...."
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyUp={searchQueryHandler}
+            />
+            <button type="submit">Search</button>
+          </form>
+        </div>
+      </ContentWrapper>
+    </div>
+  );
+};
+
+export default HeroBanner;
